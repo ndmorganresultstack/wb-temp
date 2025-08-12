97caa02e5ac54bf446b4d2f56912aa854730b25e
Nathan Morgan <nathan.morgan@tatcs.com>
2025-08-12 14:01:06 -0500

    remove middleware

--- middleware.ts
+++
@@ -1,6 +0,0 @@
- export { auth as middleware } from "@/auth"
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.swa|api/users|api/auth).*)', // Added |api/auth to prevent checks on auth callbacks
-  ],
-};
