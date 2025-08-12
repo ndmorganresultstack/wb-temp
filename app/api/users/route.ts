99fd3027a9e9f4c0e99f7a3eb42b83297c8345b1
Nathan Morgan <nathan.morgan@tatcs.com>
2025-08-12 11:00:50 -0500

    added more error info

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -22,6 +22,6 @@ export async function GET(request: NextRequest) {
     return NextResponse.json(!!userFound, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Failed to query for user' }, { status: 500 });
+    return NextResponse.json({ error: `Failed to query for user ${error}` }, { status: 500 });
   }
 }
