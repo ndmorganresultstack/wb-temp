789e6f8b25b80595f491edea152eb871623d7054
Nathan Morgan <nathan.morgan@tatcs.com>
2025-08-27 21:44:00 -0500

    Code Cleanup

--- app/external-labor/page.tsx
+++ app/external-labor/page.tsx
@@ -1,35 +1,37 @@
-'use client';
+"use client";
 
-import DynamicTable from '@/components/DynamicTable';
-import '../../app/globals.css';
-import { useSidebar } from '../sidebarContext';
-import { useEffect } from 'react';
+import DynamicTable from "@/components/DynamicTable";
+import "../../app/globals.css";
+import { useEffect } from "react";
+import { useNav } from "@/hooks/useNav";
+import { SiteHeader } from "@/components/SiteHeader";
 
 export default function EmployeesPage() {
+	const isSidebarOpen = useNav((state) => state.isSidebarOpen);
+	const sidebarMaxWidth = useNav((state) => state.sidebarMaxWidth);
+	const sidebarMinWidth = useNav((state) => state.sidebarMinWidth);
 
-  const {isSidebarOpen,sidebarMaxWidth,sidebarMinWidth, setPageTitle, pageTitle} = useSidebar();
- 
-  useEffect(()=>{
-    if(pageTitle !== "External Labor"){
-        setPageTitle("External Labor");
-    }
-    
-
-  },[])
-
-  return (
-    <div 
-        className={`grid-page-container ${
-          isSidebarOpen
-            ? `w-[calc(100%-${sidebarMaxWidth})]`
-            : `w-[calc(100%-${sidebarMinWidth})]`
-        }`} > 
-      <div className="grid-page-header">
-        <span className="grid-page-header-path"> Costs /</span><span className="grid-page-header-page"> External Labor</span> 
-      </div> 
-      <div className="grid-container" style={{height:'calc(100%)', width:'calc(100%)' }}>
-        <DynamicTable model="ExternalLabor" />
-      </div>
-    </div>   
-  );
+	return (
+		<>
+			<SiteHeader pageTitle="Software Costs" />
+			<main
+				className={`grid-page-container ${
+					isSidebarOpen
+						? `w-[calc(100%-${sidebarMaxWidth})]`
+						: `w-[calc(100%-${sidebarMinWidth})]`
+				}`}
+			>
+				<div className="grid-page-header">
+					<span className="grid-page-header-path"> Costs /</span>
+					<span className="grid-page-header-page"> External Labor</span>
+				</div>
+				<div
+					className="grid-container"
+					style={{ height: "calc(100%)", width: "calc(100%)" }}
+				>
+					<DynamicTable model="ExternalLabor" readOnly={false} />
+				</div>
+			</main>
+		</>
+	);
 }
