e1747ffc60e9e48e7129dc13325ab0a7bf21f97a
Tommy Wells <tommy.wells@resultstack.com>
2025-08-29 07:59:59 -0500

    Pipeline intro section with cards

--- app/pipeline/page.tsx
+++ app/pipeline/page.tsx
@@ -7,6 +7,11 @@ import { useEffect, useState } from "react";
 import { useTableRecord } from "@/hooks/useTableRecord";
 import { PipelineOpportunityData } from "@/app/api/opportunities/route";
 import PipelineGrid from "@/components/PipelineGrid";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import SearchIcon from "@/components/icons/SearchIcon";
+import { FunnelIcon } from "@heroicons/react/24/outline";
+import { Percent, TrendingUp, Users } from "lucide-react";
+import { TableSummaryCard } from "@/components/TableSummaryCard";
 
 export default function PipelinePage() {
 	const isSidebarOpen = useNav((state) => state.isSidebarOpen);
@@ -28,7 +33,7 @@ export default function PipelinePage() {
 						setOpportunity(j);
 						setRightPanelShowing(true);
 					});
-				}
+				},
 			);
 		}
 	}, [selectedRecord]);
@@ -48,6 +53,64 @@ export default function PipelinePage() {
 					<span className="grid-page-header-path"> Client Services /</span>
 					<span className="grid-page-header-page"> Sales Pipeline </span>
 				</div>
+
+				<div className=" bg-gray-200 p-8 w-full">
+					<div className="flex gap-x-4 max-w-[1400px] mx-auto justify-around">
+						<TableSummaryCard
+							title="Total Pipeline Value"
+							value="$2.4M"
+							percentage={14}
+							isIncrease
+							Icon={TrendingUp}
+						/>
+						<TableSummaryCard
+							title="Active Proposals"
+							value="42"
+							percentage={3}
+							Icon={Users}
+						/>
+						<TableSummaryCard
+							title="Avg. Probability"
+							value="63%"
+							percentage={14}
+							isIncrease
+							Icon={Percent}
+						/>
+						<TableSummaryCard
+							title="Total Units"
+							value="12,000"
+							percentage={14}
+							isIncrease
+							Icon={Users}
+						/>
+					</div>
+				</div>
+
+				<div className="flex gap-x-10 py-2 items-center px-4 w-full justify-between">
+					<div className="flex gap-x-4">
+						<label className="relative flex items-center">
+							<SearchIcon className="absolute left-2 size-4" />
+							<input
+								placeholder="Search"
+								className="border  border-gray-200 rounded-xs py-1 pl-7 pr-2"
+							/>
+						</label>
+
+						<select className="border border-gray-200  rounded-xs min-w-[150px] py-1 text-sm px-2">
+							<option>All Stages</option>
+						</select>
+
+						<button className="button button-outline text-sm flex gap-x-2 items-center">
+							Clear Filters <FunnelIcon className="size-4" />
+						</button>
+					</div>
+
+					<button className="button button-solid text-sm flex items-center gap-x-1">
+						<PlusIcon className="size-3" />
+						Add Prospect
+					</button>
+				</div>
+
 				<div className="grid-container-toolbar">
 					<PipelineGrid />
 
---
+++ components/TableSummaryCard.tsx
@@ -0,0 +1,39 @@
+import { type LucideProps } from "lucide-react";
+
+interface TableSummaryCardProps {
+	title: string;
+	value: string;
+	percentage: number;
+	isIncrease?: boolean;
+	Icon: React.ForwardRefExoticComponent<
+		Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+	>;
+}
+
+export function TableSummaryCard({
+	title,
+	value,
+	percentage,
+	isIncrease,
+	Icon,
+}: TableSummaryCardProps) {
+	return (
+		<div className="bg-white text-xs rounded-md w-full min-w-[200px] border border-gray-300 shadow-xs p-3 flex flex-col gap-y-2">
+			<div className="flex gap-x-5 justify-between items-center">
+				<span>{title}</span>
+				<Icon className="size-5 bg-gray-200 rounded-xs p-0.5" />
+			</div>
+			<div className="flex gap-x-5 items-center">
+				<b className="text-lg italic">{value}</b>
+				<div
+					className={`${
+						isIncrease ? "bg-green-200" : "bg-red-200"
+					} rounded-sm px-1 py-0.5`}
+				>
+					{isIncrease ? "+" : "-"}
+					{percentage}%
+				</div>
+			</div>
+		</div>
+	);
+}
